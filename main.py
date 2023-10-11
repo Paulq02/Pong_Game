186fd816a0a602d799cded9eb48ff7a27f9c05ef
Paulq02 <115184189+Paulq02@users.noreply.github.com>
2023-10-10 18:31:31 -0700

    Add files via upload
    added paddle class

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+import time
 
 
+
+
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Paul Pong Game")
+screen.tracer(0)
+
+
+paddle = Paddle()
+
+
+
+
+screen.listen()
+screen.onkey(paddle.move_up, "Up")
+screen.onkey(paddle.move_down, "Down")
+
+
+game_on = True
+while game_on:
+    screen.update()
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
